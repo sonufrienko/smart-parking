a69344a7f53108be06c2947f39cd737f613a61ba
Sergey Onufrienko <sergey@onufrienko.com>
2019-07-24 11:52:03 +0300

    Add API and Auth

---
+++ src/API.ts
@@ -0,0 +1,151 @@
+/* tslint:disable */
+//  This file was automatically generated and should not be edited.
+
+export type CreateTodoInput = {
+  id?: string | null,
+  name: string,
+  description?: string | null,
+};
+
+export type UpdateTodoInput = {
+  id: string,
+  name?: string | null,
+  description?: string | null,
+};
+
+export type DeleteTodoInput = {
+  id?: string | null,
+};
+
+export type ModelTodoFilterInput = {
+  id?: ModelIDFilterInput | null,
+  name?: ModelStringFilterInput | null,
+  description?: ModelStringFilterInput | null,
+  and?: Array< ModelTodoFilterInput | null > | null,
+  or?: Array< ModelTodoFilterInput | null > | null,
+  not?: ModelTodoFilterInput | null,
+};
+
+export type ModelIDFilterInput = {
+  ne?: string | null,
+  eq?: string | null,
+  le?: string | null,
+  lt?: string | null,
+  ge?: string | null,
+  gt?: string | null,
+  contains?: string | null,
+  notContains?: string | null,
+  between?: Array< string | null > | null,
+  beginsWith?: string | null,
+};
+
+export type ModelStringFilterInput = {
+  ne?: string | null,
+  eq?: string | null,
+  le?: string | null,
+  lt?: string | null,
+  ge?: string | null,
+  gt?: string | null,
+  contains?: string | null,
+  notContains?: string | null,
+  between?: Array< string | null > | null,
+  beginsWith?: string | null,
+};
+
+export type CreateTodoMutationVariables = {
+  input: CreateTodoInput,
+};
+
+export type CreateTodoMutation = {
+  createTodo:  {
+    __typename: "Todo",
+    id: string,
+    name: string,
+    description: string | null,
+  } | null,
+};
+
+export type UpdateTodoMutationVariables = {
+  input: UpdateTodoInput,
+};
+
+export type UpdateTodoMutation = {
+  updateTodo:  {
+    __typename: "Todo",
+    id: string,
+    name: string,
+    description: string | null,
+  } | null,
+};
+
+export type DeleteTodoMutationVariables = {
+  input: DeleteTodoInput,
+};
+
+export type DeleteTodoMutation = {
+  deleteTodo:  {
+    __typename: "Todo",
+    id: string,
+    name: string,
+    description: string | null,
+  } | null,
+};
+
+export type GetTodoQueryVariables = {
+  id: string,
+};
+
+export type GetTodoQuery = {
+  getTodo:  {
+    __typename: "Todo",
+    id: string,
+    name: string,
+    description: string | null,
+  } | null,
+};
+
+export type ListTodosQueryVariables = {
+  filter?: ModelTodoFilterInput | null,
+  limit?: number | null,
+  nextToken?: string | null,
+};
+
+export type ListTodosQuery = {
+  listTodos:  {
+    __typename: "ModelTodoConnection",
+    items:  Array< {
+      __typename: "Todo",
+      id: string,
+      name: string,
+      description: string | null,
+    } | null > | null,
+    nextToken: string | null,
+  } | null,
+};
+
+export type OnCreateTodoSubscription = {
+  onCreateTodo:  {
+    __typename: "Todo",
+    id: string,
+    name: string,
+    description: string | null,
+  } | null,
+};
+
+export type OnUpdateTodoSubscription = {
+  onUpdateTodo:  {
+    __typename: "Todo",
+    id: string,
+    name: string,
+    description: string | null,
+  } | null,
+};
+
+export type OnDeleteTodoSubscription = {
+  onDeleteTodo:  {
+    __typename: "Todo",
+    id: string,
+    name: string,
+    description: string | null,
+  } | null,
+};
