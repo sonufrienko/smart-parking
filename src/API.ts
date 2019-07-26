5b7742ffcab9df8038a5228112ab7c4cccff95b7
Sergey Onufrienko <sergey@onufrienko.com>
2019-07-26 10:27:51 +0300

    Update GraphQL schema

--- src/API.ts
+++ src/API.ts
@@ -1,6 +1,25 @@
 /* tslint:disable */
 //  This file was automatically generated and should not be edited.
 
+export type UpdateSlotStatusInput = {
+  Device: number,
+  SlotStatus: number,
+};
+
+export type UpdateSlotStatusMutationVariables = {
+  input: UpdateSlotStatusInput,
+};
+
+export type UpdateSlotStatusMutation = {
+  updateSlotStatus:  {
+    __typename: "Slot",
+    Id: string,
+    Device: number | null,
+    SlotNumber: string | null,
+    SlotStatus: number | null,
+  } | null,
+};
+
 export type ListSlotsQuery = {
   listSlots:  Array< {
     __typename: "Slot",
--- src/graphql/mutations.ts
+++ src/graphql/mutations.ts
@@ -1,2 +1,12 @@
 // tslint:disable
 // this is an auto generated file. This will be overwritten
+
+export const updateSlotStatus = `mutation UpdateSlotStatus($input: UpdateSlotStatusInput!) {
+  updateSlotStatus(input: $input) {
+    Id
+    Device
+    SlotNumber
+    SlotStatus
+  }
+}
+`;
