87ac93949275bdc734db17d81ed09ba3917feb9b
Sergey Onufrienko <sergey@onufrienko.com>
2019-07-26 10:26:28 +0300

    Add a function - updateSlotStatus

---
+++ amplify/backend/function/updateSlotStatus/src/index.js
@@ -0,0 +1,22 @@
+const { slotService } = require('/opt/shared');
+
+const getSlotProps = item => ({
+  Id: item.PK,
+  Device: item.Device,
+  SlotNumber: item.SlotNumber,
+  SlotStatus: item.SlotStatus
+});
+
+const updateSlotStatus = async ({ Device, SlotStatus }) => {
+  const { Item } = await slotService.setStatus({ device: Device, slotStatus: SlotStatus });
+  return getSlotProps(Item);
+};
+
+exports.handler = async (event, context) => {
+  try {
+    const slot = await updateSlotStatus(event.arguments.input);
+    context.done(null, slot);
+  } catch (err) {
+    context.done(err, null);
+  }
+};
