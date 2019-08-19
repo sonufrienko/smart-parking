15aa8997cbcd87554275971656ea4af219f767d3
Sergey Onufrienko <sergey@onufrienko.com>
2019-08-19 14:24:14 +0300

    Remove console.log

--- src/state/reducers/invoices.ts
+++ src/state/reducers/invoices.ts
@@ -3,13 +3,11 @@ import { InvoiceState, InvoicesState, Action } from '../../types';
 const invoicesReducer = (invoices: InvoicesState, action: Action): InvoicesState => {
   switch (action.type) {
     case 'START_PARKING':
-      console.log(action.payload);
       const newInvoices: InvoicesState = new Map<string, InvoiceState>(invoices);
       newInvoices.set(action.payload.SlotID, action.payload);
       return newInvoices;
 
     case 'FINISH_PARKING':
-      console.log(action.payload);
       const invoicesAfterFinish: InvoicesState = new Map<string, InvoiceState>(invoices);
       invoicesAfterFinish.delete(action.payload.SlotID);      
       return invoicesAfterFinish;
